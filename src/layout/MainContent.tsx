f2c07ae50ce98544bd95e776742609940687ccfa
Robi B <berherobi@gmail.com>
2024-02-23 16:54:27 -0500

    feat: some cleanup

--- src/layout/MainContent.tsx
+++ src/layout/MainContent.tsx
@@ -4,14 +4,13 @@ import styled from "@emotion/styled";
 
 
 const Container = styled.div({
-    // padding:"16px",
     boxSizing: 'border-box',
     borderRadius: "4px",
     position: "relative",
     margin: "0 auto",
     width: "85%",
     backgroundColor: "#f0f5f5",
-    display: 'flex', //added later
+    display: 'flex',
     flexGrow: 1
 });
 
